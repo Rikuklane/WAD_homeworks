2cd73db2d16b1d3b79ebe2d2371de62cc7cb0221
Rikuklane <richard.kuklane@ut.ee>
2021-10-23 15:16:43 +0300

    js and json edit

--- res/js/script.js
+++ res/js/script.js
@@ -1,6 +1,4 @@
-//jQuery code that can retrieve/fetch the posts information from the endpoint
 // Edit endpoint from: https://www.npoint.io/docs/ca4f889b085ef8f24474
-
 
 $(document).ready(function() {
 
@@ -15,6 +13,7 @@ $(document).ready(function() {
             content.style.display = "";
         }
     })
+    //jQuery code that can retrieve/fetch the posts information from the endpoint
     $.get("https://api.npoint.io/ca4f889b085ef8f24474", function(posts) {
     //$.getJSON("res/json/posts.json", function(posts) {
         for (let post of posts) {
@@ -24,13 +23,6 @@ $(document).ready(function() {
             let post_header_div = $('<div class= "post-header">');
             let user_icon = $('<i class="fas fa-user-circle fa-2x"></i>');
             let post_date = $('<i class="post-date"></i>').text(post.date);
-
-            //if the post does have an image then get the image from the URL
-            if (post.image !== "") {
-                let post_picture = $('<img class="post-picture" alt="feed picture" src="">').attr("src", post.image)
-                post_div.append(post_picture)
-            }
-
             let post_footer_div = $('<div class= "post-footer">');
             let like_button_div = $('<div class= "like-button">');
             let like_button = $('<i class="fas fa-thumbs-up fa-lg"></i>');
@@ -45,6 +37,11 @@ $(document).ready(function() {
             post_header_div.append(post_date)
 
             post_div.append(post_header_div)
+            //if the post does have an image then get the image from the URL
+            if (post.image !== "") {
+                let post_picture = $('<img class="post-picture" alt="feed picture" src="">').attr("src", post.image)
+                post_div.append(post_picture)
+            }
             post_div.append(post_footer_div)
 
             //appending the post to the page
